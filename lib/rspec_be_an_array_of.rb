0acfff9d2301215145fa4d0af19f270eaf8a56c4
Pooyan Khosravi <pekhee@gmail.com>
2016-01-06 11:43:13 +0800

    include gem summary on high level module desc

--- lib/rspec_be_an_array_of.rb
+++ lib/rspec_be_an_array_of.rb
@@ -1,5 +1,7 @@
 require "rspec_be_an_array_of/identity"
 require "rspec_be_an_array_of/matcher"
 
+# Matches agains an array of specified type. Type is either class name or one of
+# class's ancestors.
 module RSpecBeAnArrayOf
 end
